6e3ae766e10f9ad1a9177bf757b9012bed6a7eb2
Monnerat Marc swisstopo <marc.monnerat@swisstopo.ch>
2024-10-05 20:59:46 +0200

    removing esri

---
+++ toolbox/frontend/setup.py
@@ -0,0 +1,72 @@
+import os
+from setuptools import setup
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name="tietools",
+    version=read("VERSION").strip(),
+    author="swisstopo",
+    description=(
+        "Basic ESRI ArcMap/ArcGis Pro TIE Toolbox to perform Trace Information Extraction (TIE) Analysis."
+    ),
+    long_description=read("README.md"),
+    long_description_content_type='text/markdown',
+    python_requires="~=3.9.18",
+    packages=["tietools", "tietools.scripts",],
+    license_files = ('LICENSE.txt',),
+    install_requires=[
+        "shapely",
+        "geocube",
+        "numpy",
+        "geopandas",
+        "mayavi",
+        "matplotlib",
+        "rasterio",
+        "scipy",
+        "scikit-image",
+        "dask",
+        "tqdm",
+        "gdal",
+        "pyproj",
+        "geojson",
+        "pyyaml",
+        "pyogrio",
+        "ipycytoscape",
+        "untie>=0.0.8",
+        "geocover_utils>=0.4.0",
+    ],
+    package_data={
+
+        "tietools.scripts": [
+            "scripts/symbols.tsv",
+            "scripts/*",
+        ],
+    },
+    include_package_data=True,
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: POSIX :: Linux",
+        "Topic :: Scientific/Engineering :: Image Processing",
+        "Topic :: Scientific/Engineering :: GIS",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
+    entry_points={
+        "console_scripts": [
+            "tie_analysis = tietools.scripts.tie_analysis_dask:main",
+            "tie_viewver = tietools.scripts.tie_viewer:main",
+        ],
+    },
+)
